403daeab6f93c1d7fa484ab02391b35531854975
SantiCampoamor <101577703+SantiCampoamor@users.noreply.github.com>
2022-04-07 08:51:06 -0300

    simulacro de prueba hecho correctamente

---
+++ Assets/Scripts/Punto1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Punto1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son iguales o no lo son.
+    public int num1;
+    public int num2;
+    void Start()
+
+    {
+        if (num1 == num2)
+        {
+            Debug.Log("Estos dos numeros son iguales");
+        }
+        else
+        {
+            Debug.Log("Estos dos numeros no son iguales");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Punto2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Punto2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //Crear las variables necesarias para ingresar por Inspector el precio de 3 productos y un monto de dinero disponible.
+    //Comunicar si la suma de los mismos supera o no el monto de dinero disponible.
+    //Luego indicar cuánto dinero sobra o falta.
+    public int precio1;
+    public int precio2;
+    public int precio3;
+    public int monto;
+    int vuelto;
+    int pago;
+    void Start()
+    {
+        int pago = precio1 + precio2 + precio3;
+        if (pago == monto)
+        {
+            Debug.Log("Si te alcanza para comprar los 3 productos, no te sobro nada");
+        }
+        else if(pago < monto)
+        {
+            int vuelto = monto - pago;
+            Debug.Log("Si te alcanza para comprar los 3 procutos, te sobro " + vuelto );
+        }
+        else
+        {
+            int falta = pago - monto;
+            Debug.Log("No te alcanza para comprar los 3 productos, te falta "+ falta );
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
